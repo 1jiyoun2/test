c2e3c885748fbf804aac33c9b2ae649742ad47e5
1jiyoun2 <74850279+1jiyoun2@users.noreply.github.com>
2020-11-22 16:16:15 +0900

    test 1

---
+++ info.php
@@ -0,0 +1,20 @@
+﻿<?php
+  echo("php Start <br>"); 
+?>
+<!DOCTYPE html> 
+<html lang="ko"> 
+<head>
+  <meta charset="UTF-8"> 
+  <meta http-equiv="X-UA-Compatible" content="ie-edge">
+  <title>Hello PHP</title> 
+</head> 
+<body> 
+  <?php
+    echo("Hello PHP<br>"); 
+  ?>
+</body> 
+</html> 
+
+<?php
+  echo("php END. <br>"); 
+?>
